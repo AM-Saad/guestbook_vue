c508b451da3238c67bcdafb7954886c1d69be508
abdelrhman <abdelrhmanm525@gmail.com>
2021-05-28 22:34:42 +0200

    Created message form

--- src/pages/Message.vue
+++ src/pages/Message.vue
@@ -1,11 +1,46 @@
-<template></template>
+<template>
+  <div>
+    <form
+      method="post"
+      action="/newmessage"
+      autocomplete="off"
+      @submit.prevent="create()"
+    >
+      <div class="">
+        <h3>Create New Message</h3>
+        <p class="error" style="color: red; margin-top: 10px"></p>
+
+        <div class="form-group">
+          <label for="message">Message</label>
+          <input
+            type="text"
+            id="message"
+            name="message"
+            class="form-control"
+            placeholder="Write your message.."
+            autocomplete="off"
+            v-model="message"
+          />
+        </div>
+
+        <input type="submit" value="Create" class="btn" />
+      </div>
+    </form>
+  </div>
+</template>
 
 <script>
 export default {
   name: "Message",
   data() {
-    return {};
+    return {
+        message:''
+    };
   },
+  methods:{
+      create(){
+      }
+  }
 };
 </script>
 
