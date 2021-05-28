482adc93a528fcc1c3085e1a58191d14eecd6863
abdelrhman <abdelrhmanm525@gmail.com>
2021-05-28 20:18:35 +0200

    Created Signup form

--- src/components/auth/AuthForms.vue
+++ src/components/auth/AuthForms.vue
@@ -1,12 +1,57 @@
 <template>
-
+  <div>
+    <form
+      method="post"
+      action="/form"
+      class="formSignup "
+      autocomplete="off"
+    >
+      <div class="">
+        <h3>Sign up</h3>
+        <input
+          type="text"
+          id="name-client"
+          name="name"
+          placeholder="Name"
+          autocomplete="off"
+          v-model="name"
+        />
+        <input
+          type="email"
+          id="email-client"
+          name="email"
+          placeholder="Email address"
+          autocomplete="false | unknown-autocomplete-value"
+          v-model="email"
+        />
+        <input
+          type="password"
+          id="password-client"
+          name="password"
+          placeholder="Password"
+          v-model="password"
+          autocomplete="false | unknown-autocomplete-value"
+        />
+        <p class="signup-error" style="color: red; margin-top: 10px"></p>
+        <a class="forgotPassword">
+          Already have account
+          <b>Login</b>
+        </a>
+        <input type="submit" value="Sign up" class="loginBtn" />
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
   name: "AuthForms",
   data() {
-    return {};
+    return {
+        name:'',
+        email:'',
+        password:''
+    };
   },
 };
 </script>
