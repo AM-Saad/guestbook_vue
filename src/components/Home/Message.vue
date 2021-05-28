6e167cc10ea2550f61699db545a3defcfc2ecb79
abdelrhman <abdelrhmanm525@gmail.com>
2021-05-28 19:41:57 +0200

    Appling some styling

--- src/components/Home/Message.vue
+++ src/components/Home/Message.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="message">
+  <div class="message p-medium m-medium">
     {{ message.message }}
   </div>
 </template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="wrapper">
     <Message v-for="message in messages" :key="message._id" :message="message"></Message>
   </div>
 </template>
@@ -25,4 +25,15 @@ export default {
 </script>
 
 <style>
+.wrapper{
+  width: 70%;
+  margin: auto;
+  display: block;
+}
+.message{
+  background-color: #fff;
+  height: 100px;
+  font-size: 22px;
+  line-height: 100px;
+}
 </style>
