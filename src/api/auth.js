3560f19542b6757f46ff4511296f92c19dde45cd
abdelrhman <abdelrhmanm525@gmail.com>
2021-05-28 20:58:59 +0200

    Added login method to Auth Class

--- src/api/auth.js
+++ src/api/auth.js
@@ -18,4 +18,23 @@ export default class Auth {
 
         }
     }
+    static async login(email, password) {
+        try {
+            const res = await fetch('http://192.168.1.3:3000/login', {
+                method: 'POST',
+                body: JSON.stringify({ email: email, password: password })
+            })
+            const json = await res.json()
+
+            if (res.status == 200 || res.status == 201) {
+
+                return { msg: json.message, json, state: true, code: res.status }
+            } else {
+                return { state: false, msg: json.message, code: res.status }
+            }
+        } catch (error) {
+            return { state: false, msg: 'Something went wrong, please try again', code: 500 }
+
+        }
+    }
 }
