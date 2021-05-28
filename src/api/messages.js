6f5376786b9e190fe72a756765a1a20b2bf103f5
abdelrhman <abdelrhmanm525@gmail.com>
2021-05-29 00:41:16 +0200

    Created deleteMessage method in Message Class

--- src/api/messages.js
+++ src/api/messages.js
@@ -31,4 +31,23 @@ export default class Message {
 
         }
     }
+    static async deleteMessage(message, user) {
+        try {
+            const res = await fetch('http://localhost:3000/messages', {
+                method: 'Delete',
+                body: JSON.stringify({ message: message, user: user })
+            })
+            const json = await res.json()
+
+            if (res.status == 200 || res.status == 201) {
+
+                return { msg: json.message, json, state: true, code: res.status }
+            } else {
+                return { state: false, msg: json.message, code: res.status }
+            }
+        } catch (error) {
+            return { state: false, msg: 'Something went wrong, please try again', code: 500 }
+
+        }
+    }
 }
