536d3dbabc6f71b1dd2c60c9e8c118f2ec7770b9
abdelrhman <abdelrhmanm525@gmail.com>
2021-05-28 19:22:45 +0200

    After removing header it works fine

--- src/api/messages.js
+++ src/api/messages.js
@@ -3,10 +3,7 @@ export default class Message {
         try {
             const res = await fetch('http://192.168.1.3:3000/messages', {
                 method: 'get',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                }
+         
             })
             console.log(res);
             return await res.json()
