db76c35671fd616aa9631a7179e34b14028120e0
abdelrhman <abdelrhmanm525@gmail.com>
2021-05-28 17:04:45 +0200

    Created messages methof to fetch all messages

--- src/api/messages.js
+++ src/api/messages.js
@@ -1,3 +1,17 @@
-export default class Message{
-    
+export default class Message {
+    static async messages() {
+        try {
+            const res = await fetch('localhost:3000/messages', {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+            return await res.json()
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
 }
