806804de5047d6c1877842c847f087de4a0f6dd3
abdelrhman <abdelrhmanm525@gmail.com>
2021-05-28 18:51:53 +0200

    Cannot fetched the messages pending

--- src/api/messages.js
+++ src/api/messages.js
@@ -1,13 +1,14 @@
 export default class Message {
     static async messages() {
         try {
-            const res = await fetch('localhost:3000/messages', {
+            const res = await fetch('http://192.168.1.3:3000/messages', {
                 method: 'GET',
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json'
                 }
             })
+            console.log(res);
             return await res.json()
         } catch (error) {
             console.log(error);
