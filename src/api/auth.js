bc5745fcc5aaa3c8f19ef216bcde8df8a37cd9b6
abdelrhman <abdelrhmanm525@gmail.com>
2021-05-28 20:25:04 +0200

    Created Signup method

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,3 +1,14 @@
-export default class Auth{
-    
+export default class Auth {
+    static async signup(name, email, password) {
+        try {
+            const res = await fetch('http://192.168.1.3:3000/signup', {
+                method: 'POST',
+                body: JSON.stringify({ name: name, email: email, password: password })
+            })
+            console.log(res);
+            return await res.json()
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
