b46406fa10e24f3c1d3e59181a7d4ec8186b7f04
abdelrhman <abdelrhmanm525@gmail.com>
2021-05-29 00:49:55 +0200

    Created deleteMsg method in Message Component

--- src/components/Home/Message.vue
+++ src/components/Home/Message.vue
@@ -2,13 +2,19 @@
   <div class="message p-medium m-medium flex f-space-between">
     {{ message.message }}
     <div class="flex" v-if="creator">
-      <button class="btn btn-danger">Delete</button>
+      <button
+        class="btn btn-danger"
+        @click="deleteMsg(message._id, message.user)"
+      >
+        Delete
+      </button>
       <button class="btn btn-info">Edit</button>
     </div>
   </div>
 </template>
 
 <script>
+import Message from "@/api/messages";
 export default {
   name: "Message",
   data() {
@@ -24,13 +30,19 @@ export default {
     }
   },
   methods: {
-    
+    async deleteMsg(id, user) {
+      const res = await Message.deleteMessage(id, user);
+      if (!res.state) {
+        return
+      }
+
+    },
   },
 };
 </script>
  
 <style scoped>
-.btn{
+.btn {
   width: unset;
 }
 </style>
